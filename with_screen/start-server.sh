6fcfdce610e8b30f26eccaabe09366d9d4375fd3
WilhelmRoscher <51135157+WilhelmRoscher@users.noreply.github.com>
2021-01-20 15:51:51 +0100

    Create start-server.sh

---
+++ with_screen/start-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create screen "minecraft"
+screen -dmS minecraft
+
+# Start the server in that screen
+screen -S minecraft -X stuff '/usr/bin/java -Xmx6G -jar /var/minecraft/server/server.jar nogui\n'
